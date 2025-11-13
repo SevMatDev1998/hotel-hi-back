b5ac5617df0423c2e281e5cc84bb113ce3763d47
Hayk Shahinyan <shahinyan142@gmail.com>
2025-11-13 14:49:27 +0400

    added countries public

--- src/modules/countries/countries.controller.ts
+++ src/modules/countries/countries.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { CountriesService } from './countries.service';
 import { CountryDto } from './dto';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('countries')
 export class CountriesController {
-  constructor(private readonly countriesService: CountriesService) {}
+  constructor(private readonly countriesService: CountriesService) { }
 
+  @Public()
   @Get()
   async findAll(): Promise<CountryDto[]> {
     return this.countriesService.findAll();
